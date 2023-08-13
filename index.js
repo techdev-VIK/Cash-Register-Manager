b383972c4352a217a6901b8d2af83bc806021669
vikartanSood <soodvikartan@gmail.com>
2023-08-14 01:54:37 +0530

    logic added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 
+const availableNotes = [2000, 1000, 500, 200, 100, 50, 20, 10, 5, 1];
 // console.log(billAmount.value);
 // console.log(cashGiven.value);
 
@@ -14,7 +15,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount(){
     if(billAmount.value > 0){
         if(cashGiven.value >= billAmount.value){
             const amountToBeReturned = cashGiven.value - billAmount.value;
-            // calculateChange(amountToBeReturned);
+            calculateChange(amountToBeReturned);
 
         }else{
             showMessage("Please provide Cash equal to or more than Billed Amount")
@@ -24,6 +25,16 @@ checkButton.addEventListener("click", function validateBillAndCashAmount(){
         showMessage("Please Enter a Valid Amount");
     }
 });
+
+function calculateChange(amountToBeReturned){
+    for(let i=0; i<availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+
+    }
+
+}
 
 function showMessage(msg){
     message.style.display ="block";
